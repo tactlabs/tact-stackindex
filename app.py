789d8b48c324bbac541b61de002baebcf32e7076
BairaviB <tbalaguru61@gmail.com>
2021-07-24 16:00:07 +0530

    api to be resolved

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask,jsonify,requests,render_template
+from flask import Flask,jsonify,request,render_template
 import json
+import stackindex
   
 app = Flask(__name__)
 PORT = 3000
   
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def startpy():
     input_col =  input("Enter input col : ")
     print(input_col)
     
     return render_template('index.html')
 
-@app.route("/result")
+@app.route("/result",methods=['POST'])
 def getplot():
-    return None
+    input_col = request.form.get('label')
+    plot = stackindex.plotdata(input_col)
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=PORT)
--- stackindex.py
+++ stackindex.py
@@ -15,7 +15,32 @@ df["month"] = pd.to_datetime(df["month"], format="%y-%b")
 #set month as index
 df = df.set_index("month")
 
-def getdata(inputcol):
-    input_col =  input("Enter name : ")
+def plotdata(input_col):
+    for column in df:
+        if input_col == column:
+            new_frame = df[column].to_frame()
+            train_len = 102
+            train = new_frame.iloc[:train_len]
+            test = new_frame.iloc[train_len:]
+            ts = TimeSeriesData(train.reset_index(), time_col_name="month")
+
+            # Specify parameters
+            params = ProphetParams(seasonality_mode="multiplicative")
+
+            # Create a model instance
+            m = ProphetModel(ts, params)
+
+            # Fit mode
+            m.fit()
+
+            # Forecast
+            fcst = m.predict(steps=50, freq="MS")
+  
+    
+
+            m.plot()
+            plt.savefig('static/figure1.png')
+            return None
+    
     
 
